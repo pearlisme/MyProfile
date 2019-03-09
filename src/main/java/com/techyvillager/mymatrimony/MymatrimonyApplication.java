6d4051d616865672a414cbab4ad8a8a4e1c4b8c2
pearl <muthubharathi.r@gmail.com>
2019-03-09 22:48:13 +0530

    Initial Commit

---
+++ src/main/java/com/techyvillager/mymatrimony/MymatrimonyApplication.java
@@ -0,0 +1,13 @@
+package com.techyvillager.mymatrimony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymatrimonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymatrimonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techyvillager/mymatrimony/MymatrimonyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techyvillager.mymatrimony;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MymatrimonyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
